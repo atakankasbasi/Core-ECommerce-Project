8b19cf9b7bf648c8692a363f1f5de761c5541972
atakan18 <atakankasbasi@gmail.com>
2023-03-21 20:05:20 +0300

    statistic linq

---
+++ SpryStore.PresentationLayer/Controllers/StatisticController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpryStore.DataAccessLayer.Concrete;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace SpryStore.PresentationLayer.Controllers
+{
+    public class StatisticController : Controller
+    {
+        public IActionResult Index()
+        {
+            Context context = new Context();
+            ViewBag.categoryCount = context.Categories.Count();
+            ViewBag.productCount = context.Products.Count();
+            ViewBag.electronicProductCount = context.Products.Where(x => x.Category.CategoryName == "Elektronik").Count(); 
+            return View();
+        }
+    }
+}
